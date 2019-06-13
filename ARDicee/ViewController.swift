3b6caa307018164cee4769bbd4565079d197c890
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-06-13 16:28:27 -0700

    Initial Commit

---
+++ ARDicee/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Bo on 6/13/19.
+//  Copyright © 2019 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        let material = SCNMaterial()
+        
+        material .diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        print("Session is supported: \(ARConfiguration.isSupported)")
+        print("Session is supported: \(ARWorldTrackingConfiguration.isSupported)")
+
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+       
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    
+    
+}
